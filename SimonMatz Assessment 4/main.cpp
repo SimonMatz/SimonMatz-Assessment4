e071e2aabd9a0e857e35218c0048b6d8d462b1b8
Simon Matz <Simon.Matz@mds.torrens.edu.au>
2021-05-01 18:11:15 +1000

    play again not working

--- SimonMatz Assessment 4/main.cpp
+++ SimonMatz Assessment 4/main.cpp
@@ -11,18 +11,19 @@ using namespace std;
 using namespace termcolor;
 using namespace textpixels;
 
-string a = "1";
-string b = "2";
-string c = "3";
-string d = "4";
-string e = "5";
-string f = "6";
-string g = "7";
-string h = "8";
-string i = "9";
+char a = '1';
+char b = '2';
+char c = '3';
+char d = '4';
+char e = '5';
+char f = '6';
+char g = '7';
+char h = '8';
+char i = '9';
 
 char player = 'x';
 bool tryAgain = false;
+int winner = 0;
 
 enum MenuChoice
 {
@@ -41,7 +42,7 @@ int displayMenuScreen()
 	while (!validChoice)
 	{
 		cout << on_red << "xxxxxxxxxx  Welcome to Tic Tac Toe  xxxxxxxxxx\n\n" << reset;
-		
+
 		cout << "\t\t" << cyan << "1" << white << ": Play\n";
 		cout << "\t\t" << cyan << "2" << white << ": Exit\n";
 		cout << "\t\t" << cyan << "3" << white << ": Credits\n";
@@ -66,13 +67,39 @@ int displayMenuScreen()
 	return choice;
 }
 
+void clearSelections()
+{
+	char a = '1';
+	char b = '2';
+	char c = '3';
+	char d = '4';
+	char e = '5';
+	char f = '6';
+	char g = '7';
+	char h = '8';
+	char i = '9';
+
+}
+
 void drawBoard()
 {
+	system("cls");
+
 	cout << "| " << a << " | " << b << " | " << c << " | " << endl;
 	cout << "|---|---|---|" << endl;
 	cout << "| " << d << " | " << e << " | " << f << " | " << endl;
 	cout << "|---|---|---|" << endl;
 	cout << "| " << g << " | " << h << " | " << i << " | " << endl;
+}
+
+void checkError()
+{
+	if (a == 'x' || a == 'o')
+	{
+		cout << "\n\n\nERROR\n\n";
+		tryAgain = true;
+		return;
+	}
 }
 
 void playerChange()
@@ -88,279 +115,86 @@ void selection()
 {
 	while (true)
 	{
-			if (keyIsDown('1'))
-			{
-				system("cls");
 
-				if (a == "x" || a == "o")
-				{
-					cout << "\n\n\nERROR\n\n";
-					tryAgain = true;
-				}
-				else
-				{
-					a = player;
-					tryAgain = false;
-				}
-
-				drawBoard();
-				system("pause");
-				system("cls");
-				return;
-			}
-
-			if (keyIsDown('2'))
-			{
-				system("cls");
-
-				if (b == "x" || b == "o")
-				{
-					cout << "\n\n\nERROR\n\n";
-					tryAgain = true;
-				}
-				else
-				{
-					b = player;
-				tryAgain = false;
-				}
-
-				drawBoard();
-				system("pause");
-				system("cls");
-				return;
-			}
-			if (keyIsDown('3'))
-			{
-				system("cls");
-
-				if (c == "x" || c == "o")
-				{
-					cout << "\n\n\nERROR\n\n";
-					tryAgain = true;
-				}
-				else
-				{
-					c = player;
-					tryAgain = false;
-				}
-				drawBoard();
-				system("pause");
-				system("cls");
-				return;
-			}
-
-			if (keyIsDown('4'))
-			{
-				system("cls");
-				if (d == "x" || d == "o")
-				{
-					cout << "\n\n\nERROR\n\n";
-					tryAgain = true;
-				}
-				else
-				{
-					d = player;
-					tryAgain = false;
-				}
-
-				drawBoard();
-				system("pause");
-				system("cls");
-				return;
-			}
-			if (keyIsDown('5'))
-			{
-				system("cls");
-				if (e == "x" || e == "o")
-				{
-					cout << "\n\n\nERROR\n\n";
-					tryAgain = true;
-				}
-				else
-				{
-					e = player;
-					tryAgain = false;
-				}
-
-				drawBoard();
-				system("pause");
-				system("cls");
-				return;
-
-			}
-
-			if (keyIsDown('6'))
-			{
-				system("cls");
-				if (f == "x" || f == "o")
-				{
-					cout << "\n\n\nERROR\n\n";
-					tryAgain = true;
-				}
-				else
-				{
-					f = player;
-					tryAgain = false;
-				}
-
-				drawBoard();
-				system("pause");
-				system("cls");
-				return;
-			}
-			if (keyIsDown('7'))
-			{
-				system("cls");
-				if (g == "x" || g == "o")
-				{
-					cout << "\n\n\nERROR\n\n";
-					tryAgain = true;
-				}
-				else
-				{
-					g = player;
-					tryAgain = false;
-				}
-
-				drawBoard();
-				system("pause");
-				system("cls");
-				return;
-			}
-
-			if (keyIsDown('8'))
-			{
-				system("cls");
-				if (h == "x" || h == "o")
-				{
-					cout << "\n\n\nERROR\n\n";
-					tryAgain = true;
-				}
-				else
-				{	h = player;
-					tryAgain = false;
-				}
-
-				drawBoard();
-				system("pause");
-				system("cls");
-				return;
-			}
-			if (keyIsDown('9'))
-			{
-				system("cls");
-				if (i == "x" || i == "o")
-				{
-					cout << "\n\n\nERROR\n\n";
-					tryAgain = true;
-				}
-				else
-				{
-					i = player;
-					tryAgain = false;
-				}
-
-				drawBoard();
-				system("pause");
-				system("cls");
-				return;
-			}
-	}
-}
-
-void selection2()
-{
-	while (true)
-	{
-		
 		if (keyIsDown('1'))
 		{
-			system("cls");
+			//system("cls");
 
-			if (a == "x" || a == "o")
+			if (a == 'x' || a == 'o')
 			{
 				cout << "\n\n\nERROR\n\n";
+				system("sleep");
 				tryAgain = true;
 			}
-
 			else
-			a = "o";
-
-			drawBoard();
-			system("pause");
-			system("cls");
+			{
+				a = player;
+				tryAgain = false;
+			}
 			return;
-
 		}
 
 		if (keyIsDown('2'))
 		{
 			system("cls");
 
-			if (b == "x" || b == "o")
+			if (b == 'x' || b == 'o')
 			{
 				cout << "\n\n\nERROR\n\n";
 				tryAgain = true;
 			}
-
 			else
-
-			b = "o";
-			
-			drawBoard();
-			system("pause");
-			system("cls");
+			{
+				b = player;
+				tryAgain = false;
+			}
 			return;
 		}
 		if (keyIsDown('3'))
 		{
 			system("cls");
-			
-			if (c == "x" || c == "o")
+
+			if (c == 'x' || c == 'o')
 			{
 				cout << "\n\n\nERROR\n\n";
 				tryAgain = true;
 			}
-
 			else
-			c = "o";
-			
-			drawBoard();
-			system("pause");
-			system("cls");
+			{
+				c = player;
+				tryAgain = false;
+			}
 			return;
 		}
 
 		if (keyIsDown('4'))
 		{
 			system("cls");
-			if (d == "x" || d == "o")
+			if (d == 'x' || d == 'o')
 			{
 				cout << "\n\n\nERROR\n\n";
 				tryAgain = true;
 			}
 			else
-			d = "o";
-		
-			drawBoard();
-			system("pause");
-			system("cls");
+			{
+				d = player;
+				tryAgain = false;
+			}
 			return;
 		}
 		if (keyIsDown('5'))
 		{
 			system("cls");
-			if (e == "x" || e == "o")
+			if (e == 'x' || e == 'o')
 			{
 				cout << "\n\n\nERROR\n\n";
 				tryAgain = true;
 			}
 			else
-			e = "o";
-			
-			drawBoard();
-			system("pause");
-			system("cls");
+			{
+				e = player;
+				tryAgain = false;
+			}
 			return;
 
 		}
@@ -368,91 +202,202 @@ void selection2()
 		if (keyIsDown('6'))
 		{
 			system("cls");
-			if (f == "x" || f == "o")
+			if (f == 'x' || f == 'o')
 			{
 				cout << "\n\n\nERROR\n\n";
 				tryAgain = true;
 			}
 			else
-			f = "o";
-			
-			drawBoard();
-			system("pause");
-			system("cls");
+			{
+				f = player;
+				tryAgain = false;
+			}
 			return;
 		}
 		if (keyIsDown('7'))
 		{
 			system("cls");
-			if (g == "x" || g == "o")
+			if (g == 'x' || g == 'o')
 			{
 				cout << "\n\n\nERROR\n\n";
 				tryAgain = true;
 			}
 			else
-			g = "o";
-			
-			drawBoard();
-			system("pause");
-			system("cls");
+			{
+				g = player;
+				tryAgain = false;
+			}
+
 			return;
 		}
 
 		if (keyIsDown('8'))
 		{
 			system("cls");
-			if (h == "x" || h == "o")
+			if (h == 'x' || h == 'o')
 			{
 				cout << "\n\n\nERROR\n\n";
 				tryAgain = true;
 			}
 			else
-			h = "o";
-			
-			drawBoard();
-			system("pause");
-			system("cls");
+			{
+				h = player;
+				tryAgain = false;
+			}
+
 			return;
 		}
 		if (keyIsDown('9'))
 		{
 			system("cls");
-			if (i == "x" || i == "o")
+			if (i == 'x' || i == 'o')
 			{
 				cout << "\n\n\nERROR\n\n";
 				tryAgain = true;
 			}
 			else
-			i = "o";
-			
-			drawBoard();
-			system("pause");
-			system("cls");
+			{
+				i = player;
+				tryAgain = false;
+			}
+
 			return;
 		}
 	}
 }
 
+void checkWinner()
+{
+	//player 1 win check
+	if (a == 'x' && b == 'x' && c == 'x')
+	{
+		winner = 1;
+		return;
+	}
+	if (d == 'x' && e == 'x' && f == 'x')
+	{
+		winner = 1;
+		return;
+	}
+	if (g == 'x' && h == 'x' && i == 'x')
+	{
+		winner = 1;
+		return;
+	}
+	if (a == 'x' && d == 'x' && g == 'x')
+	{
+		winner = 1;
+		return;
+	}
+	if (b == 'x' && e == 'x' && h == 'x')
+	{
+		winner = 1;
+		return;
+	}
+	if (c == 'x' && f == 'x' && i == 'x')
+	{
+		winner = 1;
+		return;
+	}
+	if (a == 'x' && e == 'x' && i == 'x')
+	{
+		winner = 1;
+		return;
+	}
+	if (c == 'x' && e == 'x' && g == 'x')
+	{
+		winner = 1;
+		return;
+	}
+
+	//player 2 win check
+	if (a == 'o' && b == 'o' && c == 'o')
+	{
+		winner = 2;
+		return;
+	}
+	if (d == 'o' && e == 'o' && f == 'o')
+	{
+		winner = 2;
+		return;
+	}
+	if (g == 'o' && h == 'o' && i == 'o')
+	{
+		winner = 2;
+		return;
+	}
+	if (a == 'o' && d == 'o' && g == 'o')
+	{
+		winner = 2;
+		return;
+	}
+	if (b == 'o' && e == 'o' && h == 'o')
+	{
+		winner = 2;
+		return;
+	}
+	if (c == 'o' && f == 'o' && i == 'o')
+	{
+		winner = 2;
+		return;
+	}
+	if (a == 'o' && e == 'o' && i == 'o')
+	{
+		winner = 2;
+		return;
+	}
+	if (c == 'o' && e == 'o' && g == 'o')
+	{
+		winner = 2;
+		return;
+	}
+
+	return;
+}
+
+
+
 void playTTT()
 {
-	while (true)
+	while (winner != 1 || winner != 2)
 	{
+
 		drawBoard();
 		do
 		{
+
 			selection();
+			checkWinner();
+
+			if (winner == 1)
+			{
+				system("cls");
+				cout << "Player 1 wins!!\n" << endl;
+				return;
+
+			}
+			else if (winner == 2)
+			{
+				system("cls");
+				cout << "Player 2 wins!!\n" << endl;
+				return;
+
+			}
+			drawBoard();
+
 
 		} while (tryAgain == true);
 
 		playerChange();
 	}
+	system("cls");
 
-	system("pause");
 }
 
 void main()
 {
 	int choice = 0;
+	int playAgain = 0;
 
 	do
 	{
@@ -462,13 +407,25 @@ void main()
 		if (choice == PLAY)
 		{
 			system("cls");
+
 			do
 			{
 				playTTT();
 
-			} while (true);
+				if (winner == 1 || winner == 2)
+				{
+					winner = 0;
+					clearSelections();
+					cout << "Enter 1 to play again or any other number to return to menu > ";
+					cin >> playAgain;
+					system("cls");
+				}
+
+			} while (playAgain == 1);
 		}
 	} while (choice != EXIT);
 
-	system("pause");
+	system("cls");
+	displayMenuScreen();
+
 }
